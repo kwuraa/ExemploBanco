dae829d9ca0e5e4bfe4dbd01eabd72ee76372165
kwuraa <kwuraa@gmail.com>
2024-08-31 21:38:30 -0300

    Update README

--- obj/Debug/net8.0/Exemplo POO.AssemblyInfo.cs
+++ obj/Debug/net8.0/Exemplo POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exemplo POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba561dcf3bbb5c743ec921bff4930723d987f081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b2ed900bb05304682cef66c0218723a0f34b4a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exemplo POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exemplo POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
